acf02f455e9d7c95b62b005bac43f9c32c9cc576
Vanessa <v4n3ss4ms@gmail.com>
2018-01-30 13:20:04 +0100

    .

--- exercises/bus/main.js
+++ exercises/bus/main.js
@@ -5,8 +5,6 @@
 var getIncomingBusesToStopUrl = 'https://openbus.emtmadrid.es/emt-proxy-server/last/geo/GetArriveStop.php';
 var server = 'https://servicios.emtmadrid.es:8443/';
 var data = {
-    idClient: 'noemims@gmail.com',
-    passKey: 'B434D627-6F9A-4C54-BEF1-86DA45ECAE1C'
 
 };
 var jqxhr = $.ajax({
