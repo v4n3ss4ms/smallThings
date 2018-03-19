8c2b545f2741e3923097f674a6296c68afa5e80c
Vanessa <v4n3ss4ms@gmail.com>
2018-03-19 22:38:51 +0100

    Delete config.js

--- bus/config.js
+++
@@ -1,5 +0,0 @@
-// Id de cliente: WEB.SERV.noemims@gmail.com
-// Pass Key: B434D627-6F9A-4C54-BEF1-86DA45ECAE1C
-// https://servicios.emtmadrid.es:8443/
-var idClient = 'WEB.SERV.noemims@gmail.com';
-var passKey ='B434D627-6F9A-4C54-BEF1-86DA45ECAE1C';
