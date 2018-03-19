c6bf1a8dd99055b9941231e3c93fb681c0065caf
Vanessa <v4n3ss4ms@gmail.com>
2018-03-19 22:37:14 +0100

    saving

--- bus/main.js
+++ bus/main.js
@@ -1,5 +1,6 @@
 
-var server = 'https://servicios.emtmadrid.es:8443/';
+var server = 'https://servicios.emtmadrid.es:8443/'; //info web
+var server = 'https://openbus.emtmadrid.es:9443/emt-proxy-server/last/ '; //API doc
 var data = {
     'idClient': idClient,
     'passKey': passKey,
@@ -12,7 +13,7 @@ var data = {
 var data2 = {
     'idClient': idClient,
     'passKey': passKey,
-    'idStop': '608',
+    'idStop': '2491',
     'url':'https://openbus.emtmadrid.es:9443/emt-proxy-server/last/geo/GetArriveStop.php'
 };
 
@@ -24,23 +25,56 @@ var data3 = {
     'url':'https://openbus.emtmadrid.es:9443/emt-proxy-server/last/geo/GetStopsFromStop.php'
 };
 
-var sendData = data;
+var sendData = data2;
 
-var jqxhr = $.ajax({
-        method: 'POST',
-        url: sendData.url,
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        data: sendData,
-    })
-    .done(function(response, textStatus, jqXHR) {
-        console.log(response);
-        //alert( "success" );
-    })
-    .fail(function() {
-        //alert( "error" );
-    })
-    .always(function() {
-        //alert( "complete" );
-    });
+var responseData;
+
+
+// saving response #2
+function getInfo(data) {
+ return $.ajax({
+     method: 'POST',
+     url: data.url,
+     headers: {
+         'Content-Type': 'application/x-www-form-urlencoded'
+     },
+     data: data,
+ });
+}
+
+getInfo(sendData).then((response) => responseData = response);
+
+
+
+// saving response #1
+// var getInfo = $.ajax({
+//             method: 'POST',
+//             url: sendData.url,
+//             headers: {
+//                 'Content-Type': 'application/x-www-form-urlencoded'
+//             },
+//             data: sendData,
+//         })
+//
+// getInfo.then((response) => responseData = response);
+
+
+// ajax call
+// var jqxhr = $.ajax({
+//     method: 'POST',
+//     url: sendData.url,
+//     headers: {
+//         'Content-Type': 'application/x-www-form-urlencoded'
+//     },
+//     data: sendData,
+// })
+//     .done(function(response, textStatus, jqXHR) {
+//         console.log(response);
+//         //alert( "success" );
+//     })
+//     .fail(function() {
+//         //alert( "error" );
+//     })
+//     .always(function() {
+//         //alert( "complete" );
+//     });
